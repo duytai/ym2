601ab75bb5864f97b707a2da64fd027d2583047d
Tai D. Nguyen <duytai.cse@gmail.com>
2024-12-26 16:57:42 +0800

    Update hello.py

--- sample/recipes/hello.py
+++ sample/recipes/hello.py
@@ -1,9 +1,11 @@
 from omegaconf import DictConfig
+from typing import Dict
 from ym2 import Stat
 
 class Hello:
-    def __init__(self, name: str):
+    def __init__(self, name: str, **kwargs: Dict[str, str]):
         self.name = name
+        print(kwargs)
 
     def main(self, stat: Stat):
         print(f'Hello {self.name}')
